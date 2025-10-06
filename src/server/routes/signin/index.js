17eefe78ae31a505fcb79bf80e723c5ec9614c58
Carles Andres <carlos.andres.c@gmail.com>
2025-10-06 16:49:54 +0100

    WIP: Scaffolded signin and signout routes

---
+++ src/server/routes/signin/index.js
@@ -0,0 +1,20 @@
+import { signinController } from './controller.js'
+
+/**
+ * Sets up the routes used in the /signin page.
+ * These routes are registered in src/server/router.js.
+ */
+export const signin = {
+  plugin: {
+    name: 'signin',
+    register(server) {
+      server.route([
+        {
+          method: 'GET',
+          path: '/signin',
+          ...signinController
+        }
+      ])
+    }
+  }
+}
