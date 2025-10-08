359071f11b5b6acf9eb9d2d26c6eb9135ff8e90e
Carles Andres <carlos.andres.c@gmail.com>
2025-10-08 21:18:24 +0100

    WIP Refactor plugin entry point

--- src/server/plugins/auth.js
+++ src/server/plugins/auth.js
@@ -3,24 +3,29 @@ import { getBellOptions } from '../common/helpers/auth/get-bell-options.js'
 import { getCookieOptions } from '../common/helpers/auth/get-cookie-options.js'
 import { config } from '../../config/config.js'
 
-export default {
+export const auth = {
   plugin: {
     name: 'auth',
     register: async (server) => {
-      const oidcConfig = await getOidcConfig()
+      const oidcWellKnownUrl = config.get(
+        'entraId.oidcWellKnownConfigurationUrl'
+      )
 
-      // Bell is a third-party plugin that provides a common interface for OAuth 2.0 authentication
-      // Used to authenticate users with Defra Identity and a pre-requisite for the Cookie authentication strategy
-      // Also used for changing organisations and signing out
-      server.auth.strategy('entra-id', 'bell', getBellOptions(oidcConfig))
+      if (oidcWellKnownUrl) {
+        const oidcConfig = await getOidcConfig()
+        server.auth.strategy('entra-id', 'bell', getBellOptions(oidcConfig))
+      } else {
+        server.auth.strategy('entra-id', 'bell', {
+          provider: 'github',
+          password: config.get('session.cookie.password'),
+          clientId: 'placeholder',
+          clientSecret: 'placeholder',
+          isSecure: config.get('isProduction')
+        })
+      }
 
-      // Cookie is a built-in authentication strategy for hapi.js that authenticates users based on a session cookie
-      // Used for all non-Defra Identity routes
-      // Lax policy required to allow redirection after Defra Identity sign out
       server.auth.strategy('session', 'cookie', getCookieOptions())
 
-      // Set the default authentication strategy to session
-      // All routes will require authentication unless explicitly set to 'entra-id' or `auth: false`
       server.auth.default('session')
     }
   }
