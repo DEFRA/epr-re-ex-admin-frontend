7e3d55082e0dfaea0fafb0fad1bf3d647b8e48cd
Carles Andrés <carlos.andres.c@gmail.com>
2025-10-13 09:36:51 +0100

    PAE-300: Repurpose "about" page as organisations page (#43)

---
+++ src/server/routes/organisations/index.js
@@ -0,0 +1,20 @@
+import { organisationsController } from './controller.js'
+
+/**
+ * Sets up the routes used in the /organisations page.
+ * These routes are registered in src/server/router.js.
+ */
+export const organisations = {
+  plugin: {
+    name: 'organisations',
+    register(server) {
+      server.route([
+        {
+          method: 'GET',
+          path: '/organisations',
+          ...organisationsController
+        }
+      ])
+    }
+  }
+}
