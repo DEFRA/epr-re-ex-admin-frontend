73055062fafe936cc4b8a105f1a342b846dc0674
Carles Andres <carlos.andres.c@gmail.com>
2025-10-15 22:29:06 +0100

    Fix small "software quality" warnings from SonarQube

--- src/server/common/helpers/auth/get-user-session.js
+++ src/server/common/helpers/auth/get-user-session.js
@@ -5,10 +5,8 @@ export async function getUserSession(request) {
     const sessionCookieName = config.get('session.cache.name')
     const userSession = await request.yar.get(sessionCookieName)
 
-    // TO-DO: Check validations on sessionId
     return userSession
-  } catch (error) {
-    // Return null if session retrieval fails (e.g., during error handling)
+  } catch {
     return null
   }
 }
