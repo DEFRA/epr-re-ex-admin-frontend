610552f38c4440442b66a596b35f3deeb14cf51f
Carles Andres <carlos.andres.c@gmail.com>
2025-10-31 09:17:05 +0000

    WIP Fix error using wreck

--- src/server/common/helpers/fetch-json-from-backend.js
+++ src/server/common/helpers/fetch-json-from-backend.js
@@ -4,7 +4,7 @@ import { getUserSession } from './auth/get-user-session.js'
 import { handleApiResponse } from './handle-api-response.js'
 
 /**
- * Fetch JSON from a given path in th
+ * Fetch JSON from a given path in the backend service.
  * @param {import('@hapi/hapi').Request} request
  * @param {string} url
  * @param {Wreck.options} options
@@ -14,20 +14,19 @@ export const fetchJsonFromBackend = async (request, path, options) => {
   const eprBackendUrl = config.get('eprBackendUrl')
   const userSession = await getUserSession(request)
 
-  const { method = 'GET', ...restOptions } = options || {}
+  const method = (options?.method || 'get').toLowerCase()
 
   const completeOptions = {
-    ...restOptions,
+    ...options,
     json: true,
     headers: {
-      ...restOptions?.headers,
+      ...options?.headers,
       Authorization: `Bearer ${userSession?.token}`,
       'Content-Type': 'application/json'
     }
   }
 
-  const { res, payload } = await Wreck.request(
-    method,
+  const { res, payload } = await Wreck[method](
     `${eprBackendUrl}${path}`,
     completeOptions
   )
