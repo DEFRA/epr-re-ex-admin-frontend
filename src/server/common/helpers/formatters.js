ab4c60358e0301ab597f51edca5c94d17f64ee18
Senthil Kumar Arumugam <senthil.arumugam@equalexperts.com>
2026-02-09 16:05:05 +0000

    PAE-1014: sonar fix

--- src/server/common/helpers/formatters.js
+++ src/server/common/helpers/formatters.js
@@ -8,6 +8,8 @@ const dateTimeFormat = "d MMMM yyyy 'at' h:mmaaa"
  * @returns {string} Formatted date string (e.g., "6 February 2026 at 2:30pm") or empty string if no date
  */
 export function formatDateTime(isoString) {
-  if (!isoString) return ''
+  if (!isoString) {
+    return ''
+  }
   return formatDate(new Date(isoString), dateTimeFormat)
 }
