252e0aff497e889fbee13b9cc7d7acf644e73d2f
David Lusby <1230979+loveboat@users.noreply.github.com>
2026-02-26 12:29:00 +0000

    fix trailing comma in lint-staged

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,5 +1,5 @@
 export default {
   '*.{js,json,md}': 'prettier --write',
   'src/**/*.js': ['npm run lint:js:fix'],
-  '*': () => 'gitleaks protect --staged --no-banner --verbose',
+  '*': () => 'gitleaks protect --staged --no-banner --verbose'
 }
