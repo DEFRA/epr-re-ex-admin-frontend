65b130f2e3a2a4a3722cce72d70533df310c0da5
Graeme Foster <80714+GraemeF@users.noreply.github.com>
2026-04-13 17:39:18 +0100

    Refactor navigation into data-driven pattern

--- src/config/nunjucks/context/build-navigation.js
+++ src/config/nunjucks/context/build-navigation.js
@@ -1,63 +1,23 @@
-export function buildNavigation(request) {
-  const navigation = [
-    {
-      text: 'Home',
-      href: '/',
-      current: request?.path === '/'
-    },
-    {
-      text: 'Organisations',
-      href: '/organisations',
-      current: request?.path === '/organisations'
-    },
-    {
-      text: 'Linked organisations',
-      href: '/linked-organisations',
-      current: request?.path === '/linked-organisations'
-    },
-    {
-      text: 'Public register',
-      href: '/public-register',
-      current: request?.path === '/public-register'
-    },
-    {
-      text: 'Tonnage monitoring',
-      href: '/tonnage-monitoring',
-      current: request?.path === '/tonnage-monitoring'
-    },
-    {
-      text: 'Waste balance availability',
-      href: '/waste-balance-availability',
-      current: request?.path === '/waste-balance-availability'
-    },
-    {
-      text: 'Summary log uploads',
-      href: '/summary-log',
-      current: request?.path === '/summary-log'
-    },
-    {
-      text: 'Overseas sites',
-      href: '/overseas-sites',
-      current:
-        request?.path === '/overseas-sites' ||
-        request?.path?.startsWith('/overseas-sites/')
-    },
-    {
-      text: 'PRN activity',
-      href: '/prn-activity',
-      current: request?.path === '/prn-activity'
-    },
-    {
-      text: 'PRN tonnage',
-      href: '/prn-tonnage',
-      current: request?.path === '/prn-tonnage'
-    },
-    {
-      text: 'System logs',
-      href: '/system-logs',
-      current: request?.path === '/system-logs'
-    }
-  ]
+const navItems = [
+  { text: 'Home', href: '/' },
+  { text: 'Organisations', href: '/organisations' },
+  { text: 'Linked organisations', href: '/linked-organisations' },
+  { text: 'Public register', href: '/public-register' },
+  { text: 'Tonnage monitoring', href: '/tonnage-monitoring' },
+  { text: 'Waste balance availability', href: '/waste-balance-availability' },
+  { text: 'Summary log uploads', href: '/summary-log' },
+  { text: 'Overseas sites', href: '/overseas-sites', matchSubPaths: true },
+  { text: 'PRN activity', href: '/prn-activity' },
+  { text: 'PRN tonnage', href: '/prn-tonnage' },
+  { text: 'System logs', href: '/system-logs' }
+]
 
-  return navigation
+export function buildNavigation(request) {
+  return navItems.map(({ text, href, matchSubPaths }) => ({
+    text,
+    href,
+    current:
+      request?.path === href ||
+      (matchSubPaths === true && request?.path?.startsWith(`${href}/`))
+  }))
 }
