5027d16e9ec668190cafd72dd73d27ef5c34fbd0
will.crossland <4037089+crosslandwa@users.noreply.github.com>
2026-04-21 16:25:39 +0100

    Multiline typedef

--- src/server/common/helpers/fetch-organisation-overview.js
+++ src/server/common/helpers/fetch-organisation-overview.js
@@ -1,9 +1,27 @@
 import { fetchJsonFromBackend } from '#server/common/helpers/fetch-json-from-backend.js'
 
 /**
- * @typedef {{ id: string, accreditationNumber: string, status: string }} Accreditation
- * @typedef {{ id: string, registrationNumber: string, status: string, material: string, site: string, processingType: string, accreditation?: Accreditation }} Registration
- * @typedef {{ id: string, companyName: string, registrations: Registration[] }} OrganisationOverview
+ * @typedef {{
+ *   id: string,
+ *   accreditationNumber: string,
+ *   status: string
+ * }} Accreditation
+ *
+ * @typedef {{
+ *   id: string,
+ *   registrationNumber: string,
+ *   status: string,
+ *   material: string,
+ *   site: string,
+ *   processingType: string,
+ *   accreditation?: Accreditation
+ * }} Registration
+ *
+ * @typedef {{
+ *   id: string,
+ *   companyName: string,
+ *   registrations: Registration[]
+ * }} OrganisationOverview
  */
 
 /**
