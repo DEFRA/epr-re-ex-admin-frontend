69b7840b0e55c68d9ee0582cf8e6e29b3d9b116d
David Lusby <1230979+loveboat@users.noreply.github.com>
2026-04-30 13:13:16 +0100

    PAE-1412: drop dead err serializer (ecsFormat bypasses pino serializers)

--- src/server/common/helpers/logging/logger-options.js
+++ src/server/common/helpers/logging/logger-options.js
@@ -3,10 +3,6 @@ import { getTraceId } from '@defra/hapi-tracing'
 
 import { config, isProductionEnvironment } from '#config/config.js'
 
-/**
- * @typedef {Error & {isBoom: true, output: {statusCode: number, payload: object}, data?: object}} BoomError
- */
-
 const logConfig = config.get('log')
 const serviceName = config.get('serviceName')
 const serviceVersion = config.get('serviceVersion')
@@ -46,38 +42,6 @@ export const loggerOptions = {
   level: logConfig.level,
   ...formatters[logConfig.format],
   nesting: true,
-  serializers: {
-    /** @param {unknown} err */
-    err: (err) => {
-      if (!(err instanceof Error)) {
-        return err
-      }
-
-      const errorObj = {
-        message: err.message,
-        stack_trace: err.stack,
-        type: err.name
-      }
-
-      // @ts-ignore - check for Boom error before casting
-      if (!isProductionEnvironment() && err.isBoom && err.output) {
-        /** @type {BoomError} */
-        const boomErr = /** @type {BoomError} */ (err)
-        errorObj.statusCode = boomErr.output.statusCode
-        errorObj.payload = boomErr.output.payload
-
-        if (boomErr.data) {
-          try {
-            errorObj.message = `${err.message} | data: ${JSON.stringify(boomErr.data)}`
-          } catch {
-            errorObj.message = `${err.message} | data: [unserializable]`
-          }
-        }
-      }
-
-      return errorObj
-    }
-  },
   mixin() {
     const mixinValues = {}
     const traceId = getTraceId()
