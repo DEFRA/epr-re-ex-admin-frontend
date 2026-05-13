bcb3aa4649479cb52fcd0214dc9f567ce6701feb
TimRutter <tim.rutter@devsectr.com>
2026-05-13 14:56:35 +0100

    feat(ors-upload): guard GET upload route with admin.write scope

--- src/server/routes/ors-upload/index.js
+++ src/server/routes/ors-upload/index.js
@@ -3,7 +3,10 @@ import { orsDownloadController } from './controller.download.js'
 import { orsListGetController } from './controller.list.get.js'
 import { orsUploadStatusGetController } from './controller.status.get.js'
 import { orsUploadRoutes } from './constants.js'
+import { requireScope } from '#server/common/helpers/auth/require-scope.js'
 import Joi from 'joi'
+
+const requireWrite = [requireScope('admin.write')]
 
 export const orsUpload = {
   plugin: {
@@ -28,6 +31,7 @@ export const orsUpload = {
           path: orsUploadRoutes.uploads,
           ...orsUploadGetController,
           options: {
+            pre: requireWrite,
             app: { pageTitle: 'Upload ORS workbooks' }
           }
         },
